afa793de6ac1502b5bdf9256ebf9fc4cbb40d361
DroidBait66 <Alpbberrak@gmail.com>
2025-08-20 10:58:01 -0400

    Finsished Scanner

--- SimplePortScanner.py
+++ SimplePortScanner.py
@@ -1,7 +1,7 @@
 import socket
 from datetime import datetime
 
-def scanner()
+def scanner():
   # Get target host from user
   target = input("Enter target IP or hostname")
 
@@ -18,5 +18,15 @@ def scanner()
   
   # Scan a range of ports
   # For now ports 1 - 1024
+  openPorts = []
   
-  
+  for port in range(1,1025):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socket.setdefaulttimeout(0.5)
+    result = sock.connect_ex((targetIP, port))
+    if result == 0:
+      print(f"Port {port} is OPEN")
+      openPorts.append(port)
+    sock.close()
+  print(f"\nScanning finished at: {datetime.now()}")  
+  print(f"Open ports: {openPorts}")
